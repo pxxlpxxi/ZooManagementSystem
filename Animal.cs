197e2426e67978ec31a4cd4436fad2a9e21050be
pxxlpxxi <vaikeflyger@hotmail.com>
2025-08-21 13:04:25 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooManagementSystem
+{
+    abstract class Animal
+    {
+        private string _name;
+        private string _species;
+        private DateTime _birthdate;
+
+        public string Name { get { return _name; } set { _name = value; } }
+        public string Species { get { return _species; } set { _species = value; } }
+        public DateTime Birthdate { get { return _birthdate; } set { _birthdate = value; } }
+
+        protected Animal(string name, string species, DateTime birthdate)
+        {
+            _name = name;
+            _species = species;
+            _birthdate = birthdate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ZooManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
